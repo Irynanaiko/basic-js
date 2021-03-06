f0ff352b07d2bdaa198f0d558a958a9389433f61
Irynanaiko <irynaiko@ukr.net>
2021-03-06 12:58:48 +0200

    feat: implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    let newArr = [];
+
+    if (!Array.isArray(members) || members.length == 0) {
+        return false;
+    }
+
+    function stringArr(members) {
+        for (var i = 0; i < members.length; i++) {
+            if (typeof members[i] != "string") {
+                continue;
+            } else {
+                newArr.push(members[i].trim().toUpperCase());
+            }
+        }
+    }
+    stringArr(members);
+    let firstLet = newArr.map(function (letter) {
+        return letter.slice(0, 1);
+    });
+
+    let result = firstLet.sort().join("");
+    return result;
 };
